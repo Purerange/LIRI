49356c436f5365f7bd2537bf5043962efd6980c5
Stephen Marsh <38229184+Purerange@users.noreply.github.com>
2018-11-05 20:06:39 -0800

    added all necessary files and the such

---
+++ liri.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+var spotifyIDs = require("./keys.js").spotify;
+
+console.log(spotifyIDs);
+
+// Collect command line arguments
+
+
+// Determine which function we need to call
+
+
+
+
+// concert-this
+// Pass the artist's name into the API and print Venue Name, Venue Location, and Event Date (using moment)
+
+
+
+
+
+
+
+
+
+
+//spotify-this-song
